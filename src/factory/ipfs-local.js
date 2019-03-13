ad3acad8682387da423de8997dd44a468e0d698d
phillmac <phillmac@users.noreply.github.com>
2019-03-14 10:57:18 +1100

    Enable pubsub by default, add default swarm addrs

--- src/factory/ipfs-local.js
+++ src/factory/ipfs-local.js
@@ -5,11 +5,23 @@ const OrbitApi  = require('../lib/orbitdb-api.js')
 
 
 function api_factory(ipfs_opts, orbitdb_dir, orbitdb_opts) {
-    let ipfs
-    let orbitdb
-    let dbm
-    let orbitdb_api
+    let ipfs, orbitdb, dbm, orbitdb_api, ipfs_defaults
 
+    ipfs_defaults = {
+        EXPERIMENTAL: {
+            pubsub: true
+        },
+        config: {
+                Addresses: {
+                Swarm: [
+                '/dnsaddr/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star',
+                '/dnsaddr/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+                ]
+            }
+        }
+    }
+
+    ipfs_opts   = Object.assign(ipfs_opts, ipfs_defaults, ipfs_opts)
     ipfs        = new Ipfs(ipfs_opts)
     orbitdb     = new OrbitDB(ipfs, orbitdb_dir, orbitdb_opts)
     dbm         = new DBManager(orbitdb)
