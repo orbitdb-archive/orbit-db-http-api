cc00d16f609e55b24b90969a0fcedce47b0a51f8
phillmac <phillmac@users.noreply.github.com>
2019-03-13 13:09:54 +1100

    Create ipfs-local.js
    Add initial code

---
+++ ipfs-local.js
@@ -0,0 +1,19 @@
+const Ipfs      = require('ipfs');
+const OrbitApi  = require('./orbitdbapi.js')
+
+
+function api_factory() {
+    var ipfs_dir = process.env.IPFS_DIR
+
+    try {
+        _ipfs = Ipfs({repo: ipfs_dir})
+        _orbitdb_api = OrbitApi()
+       Promise.resolve(_orbitdb_api.init(_ipfs)).catch((err) => {throw err})
+    } catch (e) {
+        console.error(e)
+        process.exit(1)
+    }
+    return _orbitdb_api
+}
+
+module.exports = api_factory
