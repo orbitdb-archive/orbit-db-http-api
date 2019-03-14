2d9f09632af3dc79cc3d3ef2a96aedd4bedf8895
phillmac <phillmac@users.noreply.github.com>
2019-03-14 12:44:54 +1100

    Use await instead of Promise.resolve

--- src/cli.js
+++ src/cli.js
@@ -33,57 +33,57 @@ Options:
     }
 }
 
-let orbitdb_dir, orbitdb_conf, orbitdb_opts, orbitdb_api;
+async function init () {
 
-try {
-    cli = new Cli()
-    args = cli.args()
-    orbitdb_dir =  args['--orbitdb-dir'] || process.env.ORBITDB_DIR
-    orbitdb_conf = args['--orbitdb-conf'] || process.env.ORBITDB_CONF
-    if (orbitdb_conf) {
-        fs.readFile(orbitdb_conf, 'utf8', function (err, data) {
-            if (err) throw err;
-            orbitdb_opts = JSON.parse(data);
+    let orbitdb_dir, orbitdb_conf, orbitdb_opts, orbitdb_api;
+
+    try {
+        cli = new Cli()
+        args = cli.args()
+        orbitdb_dir =  args['--orbitdb-dir'] || process.env.ORBITDB_DIR
+        orbitdb_conf = args['--orbitdb-conf'] || process.env.ORBITDB_CONF
+        if (orbitdb_conf) {
+            fs.readFile(orbitdb_conf, 'utf8', function (err, data) {
+                if (err) throw err;
+                orbitdb_opts = JSON.parse(data);
+            });
+        }
+
+        switch(true){
+            case args['local']:
+                const api_factory_local = require('./factory/ipfs-local.js');
+                let ipfs_conf, ipfs_opts;
+                ipfs_opts = {}
+                ipfs_conf = args['--ipfs-conf'] || process.env.IPFS_CONF
+                if (ipfs_conf) {
+                    fs.readFile(ipfs_conf, 'utf8', function (err, data) {
+                        if (err) throw err;
+                        ipfs_opts = JSON.parse(data);
+                    });
+                }
+                orbitdb_api = await api_factory_local(ipfs_opts, orbitdb_dir, orbitdb_opts)
+                break;
+
+            case args['api']:
+                const api_factory_remote = require('./factory/ipfs-api.js');
+                ipfs_host = args['--ipfs-host'] || process.env.IPFS_HOST;
+                if (!ipfs_host) throw new Error ('Missing IPFS_HOST');
+                ipfs_port = args['--ipfs-port'] || process.env.IPFS_PORT || 5001;
+                orbitdb_api = await api_factory_remote(ipfs_host, ipfs_port, orbitdb_dir, orbitdb_opts)
+                break;
+
+                default:
+                throw new Error('Unrecognised ipfs type');
+        }
+        api_port = args['--api-port'] || process.env.API_PORT || 3000
+        orbitdb_api.listen(api_port, () => {
+            console.log(`Server running on port ${api_port}`);
         });
+
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
     }
-
-    switch(true){
-        case args['local']:
-            const api_factory_local = require('./factory/ipfs-local.js');
-            let ipfs_conf, ipfs_opts;
-            ipfs_opts = {}
-            ipfs_conf = args['--ipfs-conf'] || process.env.IPFS_CONF
-            if (ipfs_conf) {
-                fs.readFile(ipfs_conf, 'utf8', function (err, data) {
-                    if (err) throw err;
-                    ipfs_opts = JSON.parse(data);
-                });
-            }
-            Promise.resolve(api_factory_local(ipfs_opts, orbitdb_dir, orbitdb_opts))
-                .then((val) => { orbitdb_api = val});
-            break;
-
-        case args['api']:
-            const api_factory_remote = require('./factory/ipfs-api.js');
-            ipfs_host = args['--ipfs-host'] || process.env.IPFS_HOST;
-            if (!ipfs_host) throw new Error ('Missing IPFS_HOST');
-            ipfs_port = args['--ipfs-port'] || process.env.IPFS_PORT || 5001;
-            Promise.resolve(api_factory_remote(ipfs_host, ipfs_port, orbitdb_dir, orbitdb_opts))
-                .then((val) => { orbitdb_api = val});
-            break;
-
-        default:
-            throw new Error('Unrecognised ipfs type');
-    }
-    api_port = args['--api-port'] || process.env.API_PORT || 3000
-    orbitdb_api.listen(api_port, () => {
-        console.log(`Server running on port ${api_port}`);
-    });
-
-} catch(err) {
-    console.error(err);
-    process.exit(1);
 }
 
-
-
+init()
